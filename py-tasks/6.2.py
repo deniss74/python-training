4f34b6f4f88f2dfe560e0a38c120f3100665115c
deniss74 <denis.kizhaev@gmail.com>
2018-01-26 17:04:44 +0000

    working version of 6.2 exercise

--- py-tasks/6.2.py
+++ py-tasks/6.2.py
@@ -4,3 +4,9 @@ mac = ['aabb:cc80:7000', 'aabb:dd80:7340', 'aabb:ee80:7000', 'aabb:ff80:7000']
 
 mac_cisco = []
 
+for MAC in mac:
+    MAC = MAC.replace(':', '.')
+    mac_cisco.append(MAC)
+
+print(mac_cisco)
+
